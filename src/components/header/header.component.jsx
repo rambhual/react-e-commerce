f17158156cc0403a8875f437df74fe1f3608f2b0
unknown <Rampritsahani@outlook.in>
2019-08-01 22:51:54 +0530

    adding header

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.styles.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Link to={`/shop`}>
+        <img
+          className="logo-container"
+          src="https://res.cloudinary.com/rps-technologies/image/upload/v1564472149/onlinelogomaker-073019-1303-8865-2000-transparent.png"
+          alt="saint gobain"
+        />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          Shop
+        </Link>
+        <Link className="option" to="/contact">
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
